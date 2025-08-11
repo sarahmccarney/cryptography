7e3983c980811832c3843f7f854bd61aa479e8c6
sarahmccarney <sarahmccarney1296@gmail.com>
2025-08-11 15:02:57 +0100

    update miller-rabin.py

--- RSA/miller-rabin.py
+++ RSA/miller-rabin.py
@@ -2,45 +2,66 @@ from math import gcd
 import random
 
 def miller_rabin(n, a):
-    '''Tests whether an integer a is a witness for (the compositeness of) an integer n'''
-    if not n % 2:
+    '''Tests whether an integer 'a' is a witness for (the compositeness of)
+        an integer 'n' '''
+    if n % 2 == 0:
         return "Composite"
-    if gcd(a, n) != 1:
-        if gcd(a, n) < n:
+    if 1 < gcd(a, n) < n:
             return "Composite"
-        
-    k = 1
-    while (n-1) % 2**k == 0:
+
+    k = 0
+    q = n - 1
+    while q % 2 == 0:
+        q //= 2
         k += 1
 
-    k -= 1
-
-    q = int((n-1)/(2**k))
-    new_a = pow(a, q, n)
-    a = new_a
-
-    if a % n == 1:
+    x = pow(a, q, n)
+    
+    if x == 1 or x == (n-1):
         return "Fails"
 
-    i = 0
-    while i <= k:
-        if a % n == (n-1):
+    for _ in range(k - 1):
+        x = pow(x, 2, n)
+        if x == n - 1:
             return "Fails"
-
-        a = (a**2) % n
-        i += 1
 
     return "Composite" 
 
 def PrimeTest(p):
-    '''Uses the Miller-Rabin test to determine whether an integer p is prime '''
-    a_values = random.sample(range(p), 100)
+    '''Uses the Miller-Rabin test to determine whether an integer 'p' is prime '''
+    a_values = [random.randrange(2, p-1) for _ in range(100)]
     for i in range(100):
         if miller_rabin(p, int(a_values[i])) == 'Composite':
-            return 'Not Prime'
+            return False
 
-    return('Likely a prime') 
+    return True
 
+def GeneratePrimes(power, r):
+    '''
+    Generate a list of integers `j` in the range [0, 1000] such that the 
+    number N = 2310 * (2^power + j) + r is likely to be a prime.
+
+    Args:
+    power (int): The exponent used in the expression 2^power which will 
+                determine how large the primes are.
+    
+    rel_prime (int): The constant added after multiplying by 2310, which 
+                    should be relatively prime to 2310 (2*3*5*7*11) to 
+                    increase chances of primality.
+
+    Returns:
+    list of int: A list of all integers j in [0, 1000] for which 
+    N = 2310 * (2^power + j) + r is likely prime.
+    '''
+
+    j_list = []
+    for j in range(1001):
+        K = 2**power + j
+        N = 2310*K + r
+        if PrimeTest(N):
+            j_list.append(j)
+    return j_list
+    
 
 if __name__ == "__main__" :
     # print(miller_rabin(561, 2))
@@ -48,8 +69,8 @@ if __name__ == "__main__" :
     # print(miller_rabin(172947529, 3))
     # print(miller_rabin(172947529, 23))
 
-    print(PrimeTest(172947529))
-    print(PrimeTest(479001599))
-    print(PrimeTest(87178291199))
-
-
+    # print(PrimeTest(172947529))
+    # print(PrimeTest(479001599))
+    # print(PrimeTest(87178291199))
+    
+    print(GeneratePrimes(1013, 1139))
